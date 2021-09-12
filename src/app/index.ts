463d8e74113e1e0e24d2900306b89eabbb04839e
Vaseraga <ms@shopback.com.br>
2021-09-12 20:14:35 -0300

    Finalizando teste conforme o descritivo da tabela.

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,6 +7,8 @@ import lottie from 'lottie-web'
 import Loading from '../assets/animations/67908-duck.json'
 import LoadingProductsAnimation from '../assets/animations/67856-men-1.json'
 import anime from 'animejs'
+import emailvalidator from 'email-validator'
+import { cpf as cpfvalidator } from 'cpf-cnpj-validator'
 
 const initProducts = async (link = '/frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1') => {
     const { data: { nextPage, products } } = await Axios.get(`https:/${link}`)
@@ -67,12 +69,149 @@ const LoadingProducts = () => {
     return animation
 }
 
+interface controlsForm {
+    name: string
+    validator?: (value: any) => [boolean, string]
+    isRequired: boolean
+}
+
+
+class FormValidator {
+    Form: HTMLFormElement
+    controls: controlsForm[]
+    Errors: Map<string, string> = new Map()
+    constructor (form: HTMLFormElement, controls: controlsForm[]) {
+        this.Form = form
+        this.controls = controls
+    }
+
+    get values() {
+        const obj: any = {}
+        this.controls.forEach(({
+            name,
+        }: controlsForm) => {
+            const inputControl = this.Form.querySelector(`[name="${name}"]:not([type='radio'])`)
+            const inputControlRadio = this.Form.querySelectorAll(`[name="${name}"][type='radio']`)
+
+            if (inputControl) {
+                obj[name] = (inputControl as HTMLInputElement).value
+            } else if (inputControlRadio.length) {
+                inputControlRadio.forEach((el: any) => {
+                    if (el.checked) {
+                        obj[name] = (el as HTMLInputElement).value
+                    }
+                })
+            }
+        })
+
+        return obj
+    }
+
+    setError(controlName: string, errorMessage: string) {
+        this.Errors.set(controlName, errorMessage)
+        const ErrorLabel = this.Form.querySelector(`[data-error="${controlName}"]`)
+        if (ErrorLabel) {
+            (ErrorLabel as any).classList.remove('hide');
+            (ErrorLabel as any).innerText = errorMessage
+        }
+    }
+
+    clearError(controlName: string) {
+        this.Errors.delete(controlName)
+        const ErrorLabel = this.Form.querySelector(`[data-error="${controlName}"]`)
+        if (ErrorLabel) {
+            (ErrorLabel as any).classList.add('hide');
+            (ErrorLabel as any).innerText = ''
+        }
+    }
+
+    check() {
+        this.controls.forEach(({
+            isRequired,
+            name,
+            validator
+        }: controlsForm) => {
+            const inputControl = this.Form.querySelector(`[name="${name}"]:not([type='radio'])`)
+            const inputControlRadio = this.Form.querySelectorAll(`[name="${name}"][type='radio']`)
+            if (inputControl && (isRequired && !((inputControl as HTMLInputElement).value))) {
+                this.setError(name, 'Este campo é obrigatório!')
+            } else if (inputControlRadio.length && isRequired) {
+                const checkeds = Array.from(inputControlRadio).filter((input: any) => input.checked)
+                if (!checkeds.length) this.setError(name, 'Este campo é obrigatório!')
+                if (checkeds.length) this.clearError(name)
+            } else if (validator) {
+                const value = (inputControl as HTMLInputElement).value
+                const [error, message] = validator(value)
+                if (!error) {
+                    this.setError(name, message)
+                } else {
+                    this.clearError(name)
+                }
+            } else {
+                this.clearError(name)
+            }
+        })
+    }
+}
+
 imageLoaded((document.querySelector('body') as any), () => {    
     window.addEventListener('load', async () => {
-        let nextPage = ''
+        const formOne = (document.getElementById('formOne') as HTMLFormElement)
+        const formTwo = (document.getElementById('formTwo') as HTMLFormElement)
         const inputMask =  document.getElementById('cpf')
         const nextPageButton = document.getElementById('next')
+        const FormOneController = new FormValidator(formOne, [
+            {
+                name: 'name',
+                isRequired: true
+            },
+            {
+                name: 'email',
+                isRequired: true,
+                validator(email: string) {
+                    const check = emailvalidator.validate(email)
+                    if (!check) {
+                        return [check, `Email ${email} é inválido`]
+                    }
+                    return [check, `Email ${email} é válido`]
+                }
+            },
+            {
+                name: 'cpf',
+                isRequired: true,
+                validator(cpf: string) {
+                    const check = cpfvalidator.isValid(cpf)
+                    if (!check) {
+                        return [check, `CPF ${cpf} é inválido`]
+                    }
+                    return [check, `CPF ${cpf} é válido`]
+                }
+            },
+            {
+                name: 'genrer',
+                isRequired: true
+            }
+        ])
+
+        const FormOneControllerTwo = new FormValidator(formTwo, [
+            {
+                name: 'name',
+                isRequired: true
+            },
+            {
+                name: 'email',
+                isRequired: true,
+                validator(email: string) {
+                    const check = emailvalidator.validate(email)
+                    if (!check) {
+                        return [check, `Email ${email} é inválido`]
+                    }
+                    return [check, `Email ${email} é válido`]
+                }
+            },
+        ])
         const parallaxTargets = '.parallax'
+        let nextPage = ''
         inputMask && VMasker(inputMask).maskPattern('999.999.999-99')
         new Rellax(parallaxTargets)
         nextPage = await initProducts()
@@ -85,6 +224,20 @@ imageLoaded((document.querySelector('body') as any), () => {
                 animation.destroy();
                 (document.getElementById('loadingPage') as HTMLElement).style.display = 'none'
             }
+        })
+
+        formOne.addEventListener('submit', (e: Event) => {
+            e.preventDefault()
+            FormOneController.check()
+            if (FormOneController.Errors.size) return
+            console.log(FormOneController.values)
+        })
+
+        formTwo.addEventListener('submit', (e: Event) => {
+            e.preventDefault()
+            FormOneControllerTwo.check()
+            if (FormOneControllerTwo.Errors.size) return
+            console.log(FormOneControllerTwo.values)
         })
         
         nextPageButton?.addEventListener('click', async () => {
